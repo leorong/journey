c8701112c4db61664d602715cca53b245ad75e3a
leorong <leorong@stanford.edu>
2015-03-01 23:32:40 -0800

    prototype

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('profile', { title: 'Express' });
+});
+
+router.get('/timeline', function(req, res, next) {
+  res.render('timeline', { title: 'Express' });
+});
+
+router.get('/entry', function(req, res, next) {
+  res.render('entry', { title: 'Express' });
 });
 
 module.exports = router;
